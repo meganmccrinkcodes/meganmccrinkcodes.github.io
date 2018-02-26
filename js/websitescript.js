a010dbdc27a188a16ae975c066ec2d4e2e8d04d8
meganmccrinkcodes <megan527@Megans-MacBook-Pro-6.local>
2018-02-25 20:12:58 -0600

    update
    updated background color, fixed grid to flex and changed type

--- js/websitescript.js
+++ js/websitescript.js
@@ -1,8 +1,8 @@
-$("path").mouseover (function (){
-  $(this).addClass ("colorize");
-
-
-}).mouseout(function(){
-  $("path").removeClass ("colorize");
-
-});
+// $("path").mouseover (function (){
+//   $(this).addClass ("colorize");
+//
+//
+// }).mouseout(function(){
+//   $("path").removeClass ("colorize");
+//
+// });
